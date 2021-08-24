8abeb6c63945594eb2102f26f74bd567718423c9
Jeremie <jeremie.chaloin@gmail.com>
2021-08-24 17:28:34 +0200

    Shellcheck correction

--- rootfs/healthcheck.sh
+++ rootfs/healthcheck.sh
@@ -5,8 +5,8 @@ EXITCODE=0
 wget -q -O /run/pfclient_stats.cur http://127.0.0.1:30053/ajax/stats
 
 if [[ -e "/run/pfclient_stats.cur" ]]; then
-  bytes_out_cur = "$(jq .master_server_bytes_out < /run/pfclient_stats.cur)"
-  bytes_out_old = "$(jq .master_server_bytes_out < /run/pfclient_stats.old)"
+  bytes_out_cur="$(jq .master_server_bytes_out < /run/pfclient_stats.cur)"
+  bytes_out_old="$(jq .master_server_bytes_out < /run/pfclient_stats.old)"
   
   if [[ "$bytes_out_cur" -gt "$bytes_out_old" ]]; then
     echo "HEALTHY"
